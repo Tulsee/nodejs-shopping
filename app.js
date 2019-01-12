11a57f70f08f9fa2065a995002b0c138eb704d0d
tulsee <ramtulsi022@gmail.com>
2019-01-12 21:42:24 +0545

    configuration completed

---
+++ app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path')
+var mongoose = require('mongoose'); 
+var  config = require('./config/database');
+
+//init app
+var app = express();
+
+//connection to database
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('successfully connected to database');
+});
+
+//view engine
+app.set('views', path.join(__dirname , 'views'));
+app.set('view engine', 'ejs');
+
+//set public folder
+app.use(express.static(path.join(__dirname , 'public')));
+
+app.get('/', function(req, res){
+    res.send('Tulsee ++ saroja');
+});
+
+//start server
+var PORT = 3000;
+app.listen(PORT,function(){
+    console.log("server is running at " +PORT);
+});
